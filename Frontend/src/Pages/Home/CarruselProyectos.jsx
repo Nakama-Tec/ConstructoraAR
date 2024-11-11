7bf54abebe040d553fa08829c379bb4337337324
Nahuel Rodriguez <nahufed@gmail.com>
2024-11-11 16:38:14 -0300

    creadas estructuras de componentes para la landing

---
+++ Frontend/src/Pages/Home/CarruselProyectos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarruselProyectos = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CarruselProyectos;
---
+++ Frontend/src/Pages/Institucional/Institucional.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Institucional = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Institucional;
