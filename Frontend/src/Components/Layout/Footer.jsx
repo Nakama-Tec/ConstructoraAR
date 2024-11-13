8cc89f8edc7eba7ad9cc5431665a4959901ba1ca
Nahuel Rodriguez <nahufed@gmail.com>
2024-11-12 21:16:37 -0300

    agregado estilos footer

--- Frontend/src/Components/Layout/Footer.jsx
+++ Frontend/src/Components/Layout/Footer.jsx
@@ -9,20 +9,20 @@ const Footer = () => {
                 <p><span className='AR'>AR</span>Construcciones</p>
             </div>
             <div className="w-1/3 flex justify-center space-x-4">
-                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://facebook.com" className='social-icon' target="_blank" rel="noopener noreferrer">
                     <i className="fab fa-facebook-f"></i>
                 </a>
-                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://twitter.com" target="_blank" className='social-icon'rel="noopener noreferrer">
                     <i className="fab fa-twitter"></i>
                 </a>
-                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://instagram.com" target="_blank" className='social-icon' rel="noopener noreferrer">
                     <i className="fab fa-instagram"></i>
                 </a>
             </div>
-            <div className="w-1/3 text-right">
-                <p>Tel: (123) 456-7890</p>
-                <p>Address: 123 Main St, City, Country</p>
-                <p>Email: info@example.com</p>
+            <div className="w-1/3 text-left">
+                <p><i className="fas fa-phone-alt"></i> (123) 456-7890</p>
+                <p><i className="fas fa-map-marker-alt"></i> 123 Main St, City, Country</p>
+                <p><i className="fas fa-envelope"></i> info@example.com</p>
             </div>
         </div>
     </footer>
