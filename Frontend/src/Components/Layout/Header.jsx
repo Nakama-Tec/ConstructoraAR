c2d775e10b546d33de006be6ebcfb07d2d0ca2c9
Bren Ibanez <rodrigofernandoesper@alu.frt.utn.edu.ar>
2024-11-14 17:34:26 -0300

    posicionamiento navbar

--- Frontend/src/Components/Layout/Header.jsx
+++ Frontend/src/Components/Layout/Header.jsx
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/sinfondo.svg";
+import '../../Styles/Navbar.css';
 
 import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+const navigationItems = [
+  { name: "Inicio", href: "/" },
+  { name: "Terrenos", href: "#terrenos" },
+  { name: "Construcciones", href: "#construncciones" },
+  { name: "Departamentos", href: "#departamentos" },
+  { name: "Institucional", href: "#institucional" },
 ];
 
 function classNames(...classes) {
@@ -24,6 +23,8 @@ function classNames(...classes) {
 }
 
 const Header = () => {
+  const [current, setCurrent] = useState("Inicio");
+
   return (
     <div>
       <Disclosure as="nav" className="bg-gray-800">
@@ -31,12 +32,12 @@ const Header = () => {
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex items-center">
               <img
-                className="items-center h-40 w-auto"
+                className="logo absolute h-40 w-auto"
                 src={logo}
                 alt="Logo"
               />
             </div>
-            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
               {/* Mobile menu button */}
               <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="sr-only">Open main menu</span>
@@ -50,23 +51,24 @@ const Header = () => {
                 />
               </DisclosureButton>
             </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
-              <div className="hidden sm:ml-6 sm:block">
+            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-center">
+              <div className="hidden md:ml-6 md:block">
                 <div className="flex space-x-4">
-                  {navigation.map((item) => (
-                    <a
+                  {navigationItems.map((item) => (
+                    <Link
                       key={item.name}
-                      href={item.href}
-                      aria-current={item.current ? "page" : undefined}
+                      to={item.href}
+                      aria-current={current === item.name ? "page" : undefined}
                       className={classNames(
-                        item.current
+                        current === item.name
                           ? "bg-gray-900 text-white"
                           : "text-gray-300 hover:bg-gray-700 hover:text-white",
                         "rounded-md px-3 py-2 text-sm font-medium"
                       )}
+                      onClick={() => setCurrent(item.name)}
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
@@ -74,20 +76,21 @@ const Header = () => {
           </div>
         </div>
 
-        <DisclosurePanel className="sm:hidden">
+        <DisclosurePanel className="md:hidden">
           <div className="space-y-1 px-2 pb-3 pt-2">
-            {navigation.map((item) => (
+            {navigationItems.map((item) => (
               <DisclosureButton
                 key={item.name}
-                as="a"
-                href={item.href}
-                aria-current={item.current ? "page" : undefined}
+                as={Link}
+                to={item.href}
+                aria-current={current === item.name ? "page" : undefined}
                 className={classNames(
-                  item.current
+                  current === item.name
                     ? "bg-gray-900 text-white"
                     : "text-gray-300 hover:bg-gray-700 hover:text-white",
                   "block rounded-md px-3 py-2 text-base font-medium"
                 )}
+                onClick={() => setCurrent(item.name)}
               >
                 {item.name}
               </DisclosureButton>
