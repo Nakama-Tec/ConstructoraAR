98a6e41b975817b09503d9f270317c85e7453d7d
Nahuel Rodriguez <nahufed@gmail.com>
2024-11-17 15:07:46 -0300

    agregadas preguntas frecuentes

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import './home.css'
 import CarruselProyectos from '../Home/CarruselProyectos'
+import PreguntasFrecuentes from './PreguntasFrecuentes'
 
 const Home = () => {
   return (
     <>
       <CarruselProyectos />
-
+      <PreguntasFrecuentes/>
     </>
   )
 }
