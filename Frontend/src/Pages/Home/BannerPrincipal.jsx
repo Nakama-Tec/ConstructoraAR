07b48afe7db43b2dddcc79b983b086109c1585c0
Nahuel Rodriguez <nahufed@gmail.com>
2024-11-17 16:20:50 -0300

    agregado banner principal

--- Frontend/src/Pages/Home/BannerPrincipal.jsx
+++ Frontend/src/Pages/Home/BannerPrincipal.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 
-const BannerPrincipal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    import { useEffect, useState } from 'react';
 
-export default BannerPrincipal;
+    const images = [
+        'https://www.comunicaciontucuman.gob.ar/fotos/notas/2023/09/26/230926094234_56125.jpeg',
+        'https://monterizos.com.ar/wp-content/uploads/2022/01/hospital-general-lamadrid-monteros.jpg',
+        'https://www.tribunaldecuentas.gob.ar/wp-content/uploads/2023/03/CDI-Burruyacu-SEOP-01.jpg',
+        'https://basicapartment.com/storage/blog/inner_page_desktop/Irlswh5WN6jMiMJroJlokMGqtxcJBWo5FLrbyzOU.jpg?v=1693814124'
+    ];
+
+    const BannerPrincipal = () => {
+        const [currentIndex, setCurrentIndex] = useState(0);
+
+        useEffect(() => {
+            const interval = setInterval(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+            }, 5000);
+            return () => clearInterval(interval);
+        }, []);
+
+        return (
+            <div className="BannerPrincipal overflow-hidden">
+            {images.map((image, index) => (
+                <div
+                key={index}
+                className={`absolute inset-0 imagenBannerPrincipal transition-opacity duration-1000 ${index === currentIndex ? 'opacity-100' : 'opacity-0'}`}
+                >
+                <img src={image} alt={`Banner ${index + 1}`} className="w-full h-full object-cover" />
+                <div className=" textoBannerPrincipal">
+                    <p>Construí tus  <span className='textoDestacado'>sueños.</span ></p>
+                    <p>Invertí en el  <span className='textoDestacado'>futuro.</span></p>
+                </div>
+                </div>
+            ))}
+            </div>
+        );
+    };
+
+    export default BannerPrincipal;
--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import './home.css'
 import CarruselProyectos from '../Home/CarruselProyectos'
 import PreguntasFrecuentes from './PreguntasFrecuentes'
+import BannerPrincipal from './BannerPrincipal'
 
 const Home = () => {
   return (
     <>
+      <BannerPrincipal />
       <CarruselProyectos />
       <PreguntasFrecuentes/>
     </>
