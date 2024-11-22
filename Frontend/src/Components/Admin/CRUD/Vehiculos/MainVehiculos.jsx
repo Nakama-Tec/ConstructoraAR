6086d6ddcb8852e1a7bfc8a847cc7012a22618eb
IsmaDevs <chavezpedrito04@gmail.com>
2024-11-22 00:22:23 -0300

    Mejora de Diseño Vehiculos

--- Frontend/src/Components/Admin/CRUD/Vehiculos/MainVehiculos.jsx
+++ Frontend/src/Components/Admin/CRUD/Vehiculos/MainVehiculos.jsx
@@ -6,6 +6,7 @@ import '../../../../Styles/table.css';
 import { URL_VEHICULOS, URL_VEHICULOS_ELIMINAR } from '../../../../Constants/endpoints-API';
 import useAuthStore from '../../../../Context/useAuthStore';
 import useVehiculoStore from '../../../../Context/useVehiculoStore';
+import Aside from '../../../Layout/Aside';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
@@ -97,7 +98,7 @@ const MainVehiculos = () => {
 
   return (
     <div>
-      <h3 className="text-white text-opacity-50">Visualizando los Vehículos</h3>
+      <p className="text-black font-semibold text-4xl display flex justify-center m-5">Registros de Vehiculos</p>
       <div className="input-search">
         <input
           className="text-black"
@@ -110,11 +111,15 @@ const MainVehiculos = () => {
       <div className="mb-4">
         <button
           onClick={openRegistroModal}
-          className="bg-green-600 text-white px-4 py-2 m-2 rounded-full transition duration-200 ease-in-out hover:bg-green-800 active:bg-green-900 focus:outline-none"
+          className="bg-green-600 text-white px-4 py-2 m-2 rounded-full transition duration-200 ease-in-out hover:bg-green-800 active:bg-green-900 focus:outline-none position relative left-64"
         >
           Registrar Vehículo
         </button>
       </div>
+      <div className='display flex'>
+        <div className='position relative top-8'>
+      <Aside/>
+        </div>
       <table className="table">
         <thead>
           {table.getHeaderGroups().map(headerGroup => (
@@ -139,17 +144,18 @@ const MainVehiculos = () => {
           ))}
         </tbody>
       </table>
+      </div>
       <div className="btn-pages">
-        <button className="m-2 p-2 bg-zinc-900 text-white h-12 rounded-[8px] font-semibold text-[16px]" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+        <button className="position relative top-1 m-2 p-2 bg-zinc-900 text-white h-12 rounded-[8px] font-semibold text-[16px]" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
           Página Anterior
         </button>
-        <button className="m-2 p-2 bg-zinc-900 text-white h-12 rounded-[8px] font-semibold text-[16px]" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+        <button className=" position relative top-1 m-2 p-2 bg-zinc-900 text-white h-12 rounded-[8px] font-semibold text-[16px]" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
           Página Siguiente
         </button>
       </div>
       <EditVehiculoModal onVehiculoEditado={getVehiculos} />
       <CrearVehiculo onVehiculoRegistrado={getVehiculos} />
-    </div>
+      </div>
   );
 };
 
