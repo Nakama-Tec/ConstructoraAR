047e926874c6e671e52404c2086babee09fdd6b7
ChocobarMatias <111932821+ChocobarMatias@users.noreply.github.com>
2024-11-22 21:48:13 -0300

    Obras

--- Frontend/src/Pages/Obras/Obras.jsx
+++ Frontend/src/Pages/Obras/Obras.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MainObras from '../../Components/Admin/CRUD/Obras/MainObras'
 
 const Obras = () => {
   return (
     <div>
-      
+      <MainObras />
     </div>
   )
 }
