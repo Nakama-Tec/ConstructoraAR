11ccec35a22e5b58a5c968f8efd599c0212a90ce
Rodrigo <esperfotografia94@gmail.com>
2024-11-26 10:43:55 -0300

    modificaciones en links del navbar, con respecto a su diseño

---
+++ Frontend/src/Pages/Construcciones/DetalleConstrucciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetalleConstrucciones = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default DetalleConstrucciones;
---
+++ Frontend/src/Pages/Departamentos/MainDepartamento.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mainDepartamento = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default mainDepartamento;
