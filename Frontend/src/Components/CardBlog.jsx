fd05d9608e065610ef82d5d2140dc1b79de20270
Bren Ibanez <rodrigofernandoesper@alu.frt.utn.edu.ar>
2024-11-28 15:45:08 -0300

    carrusel modificado, falta cambiar el tipo de letra

--- Frontend/src/Components/CardBlog.jsx
+++ Frontend/src/Components/CardBlog.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 
-
-
-const CardBlog = ({blog}) => {
-    return (
-
-        
-
-        <div className="h-full w-[300px] flex-shrink-0 cursor-pointer">
-            <div className="overflow-hidden relative h-[200px]">
-                <img src={`/imgs/blogpost${blog.id}.avif`} alt={blog.tittle}
-                className="shadow-xl brightness-50 hover:brightness-100 transition duration-300 ease-in-out"
-                />
-                {/* cambiar, debe tener su categoria en el centro, al hacer hover se agranda la palabra */}
-                <span className="absolute top-3 left-4 border border-blue-200 text-xs rounded-lg px-4 py-2 font-semibold capitalize bg-blue-100">
-                    {blog.category}
-                </span>
-            </div>
-            
-
-        </div>
-    )
-    
+const CardBlog = ({ blog }) => {
+  return (
+    <div className="h-full w-[300px] flex-shrink-0 cursor-pointer">
+      <div className="overflow-hidden relative h-[200px] group">
+        <img
+          src={`/imgs/blogpost${blog.id}.avif`}
+          alt={blog.tittle}
+          className="shadow-xl brightness-50 transition duration-300 ease-in-out transform group-hover:brightness-100 group-hover:scale-105"
+        />
+        {/* Posicionar el texto en el centro y cambiar el color a blanco */}
+        <span className="absolute inset-0 flex items-center justify-center text-xl font-semibold capitalize text-white">
+          {blog.category}
+        </span>
+      </div>
+    </div>
+  );
 };
+
 export default CardBlog;
