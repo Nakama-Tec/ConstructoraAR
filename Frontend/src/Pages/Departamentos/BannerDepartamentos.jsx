b43f52c53cfa4a050fa0d260398eb9c81b3a9cd3
Rodrigo <esperfotografia94@gmail.com>
2024-12-13 05:33:02 -0300

    banner departamentos realizao

--- Frontend/src/Pages/Departamentos/BannerDepartamentos.jsx
+++ Frontend/src/Pages/Departamentos/BannerDepartamentos.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-
+import bannerdepa1 from '../../assets/img departamento/bannerdepa.jpg';
+// import bannerdepa2 from '../../assets/img departamento/bannerdepa2.jpg';
+// import bannerdepa3 from '../../assets/img departamento/bannerdepa3.jpg';
     import { useEffect, useState } from 'react';
 
-    const images = [
-        'https://canalc.com.ar/wp-content/uploads/2024/01/construir-una-casa-desde-cero.jpg',
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmaxacero.com%2Fblog%2F14-pasos-para-la-construccion-de-una-casa-guia-desde-cero%2F&psig=AOvVaw0yLo5GV9dnwSKR1zd_2s8z&ust=1734012344584000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPCirMDxn4oDFQAAAAAdAAAAABAK',
-        'https://www.salta.gob.ar/public/images/noticias/77961-avanza-la-construccion-de-100-viviendas-en-oran.jpg',
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fes%2Fsearch%2Fconstrucci%25C3%25B3n-de-la-casa&psig=AOvVaw0yLo5GV9dnwSKR1zd_2s8z&ust=1734012344584000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPCirMDxn4oDFQAAAAAdAAAAABAd'
-    ];
+    const images = [bannerdepa1];
 
     const BannerDepartamentos = () => {
         const [currentIndex, setCurrentIndex] = useState(0);
