277399517e55d0502624cf22ebd4942ef125abad
Nahuel Rodriguez <nahufed@gmail.com>
2024-12-16 11:06:43 -0300

    se agregaron etiquetas meta y se cambio el titulo para que sea consistente con la empresa

--- Frontend/src/Components/Layout/Header.jsx
+++ Frontend/src/Components/Layout/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import logo from "../../assets/sinfondo.svg";
+import logo from "../../assets/logoconfondo-removebg-preview.png";
 import "../../Styles/Navbar.css";
 import useAuthStore from "../../Context/useAuthStore";
 import {
@@ -37,9 +37,9 @@ const Header = () => {
           <>
             <div className="contenedor-nav">
               <div className=" contenedor-nav relative flex h-16">
-                <div className="flex items-center">
+                <Link to={'/'} className="flex items-center">
                   <img className="logo h-40 w-auto " src={logo} alt="Logo" />
-                </div>
+                </Link>
                 <div className="boton-movil absolute inset-y-0 right-0 flex items-center md:hidden">
                   {/* Botón de menú móvil */}
                   <Disclosure.Button className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
